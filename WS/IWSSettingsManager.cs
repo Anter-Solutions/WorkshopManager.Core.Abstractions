5d16e57fe866637feba056475775d0789c0740f7
vladimirmazarakis <87600021+vladimirmazarakis@users.noreply.github.com>
2022-11-18 00:48:27 +0200

    WS Settings Abstract Manager.

---
+++ WS/IWSSettingsManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkshopManager.Common.WS;
+
+namespace WorkshopManager.Core.Abstractions.WS
+{
+    public interface IWSSettingsManager
+    {
+        Task<WSSettings> Get();
+        Task Update(WSSettings wss);
+    }
+}
