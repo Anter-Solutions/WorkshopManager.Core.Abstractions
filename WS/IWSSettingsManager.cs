ecf3258fbfb4f42773a9c303d96230ea3f5af2ed
vladimirmazarakis <87600021+vladimirmazarakis@users.noreply.github.com>
2022-11-18 12:08:35 +0200

    Updates.

--- WS/IWSSettingsManager.cs
+++ WS/IWSSettingsManager.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WorkshopManager.Common.WS;
+﻿using WorkshopManager.Common.WS;
 
 namespace WorkshopManager.Core.Abstractions.WS
 {
